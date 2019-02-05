f88d75b4edbf03f5c8162a43def10d89bf9d7d24
k3ntako <kentarokaneki@gmail.com>
2019-02-05 11:21:35 -0500

    Template and Autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 let socket = io();
 
+function scrollToBottom(){
+  let messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child');
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on("connect", function () {
   console.log('connected to server');
 })
@@ -10,22 +24,30 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message){
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  let li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-  jQuery('#messages').append(li);
+  let template = jQuery('#message-template').html();
+  let html = Mustache.render(template,{
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
 })
 
 socket.on('newLocationMessage', function(message){
-  console.log(message);
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  let li = jQuery('<li></li>');
-  let a = jQuery(`<a target="_blank">My current location</a>`);
-  li.text(`${message.from} ${formattedTime}: `)
-  a.attr('href', message.url);
-  li.append(a);
 
-  jQuery('#messages').append(li);
+  let template = jQuery('#location-message-template').html();
+  let html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
 })
 
 jQuery('#message-form').on('submit', function(e){
